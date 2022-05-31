c14ca5fe5ae51d7acc82368af57e2f3236864578
Yonn Argelia <yonnarge@student.21-school.ru>
2022-05-31 16:22:11 +0300

    Add s21_cat.c, Makefile, test file 'bytes.txt' in /datasets.

---
+++ src/cat/s21_cat.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (с) 2022 Student of School 21:
+ * Yonn Argelia
+ *
+ * yonnarge@student.21-school.ru
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        printf("%s", argv[1]);
+    }
+    return (0);
+}
+
