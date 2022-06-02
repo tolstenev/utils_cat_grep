dd3d69241ffce9c433f767bb94a72b56a4abf42d
Yonn Argelia <yonnarge@student.21-school.ru>
2022-06-02 20:05:05 +0300

    In progress s21_cat.c. Add first.txt in /datasets.

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -6,30 +6,48 @@
  */
 
 #include <stdio.h>
-#include "../common/s21_bool.h"
+#include <stdbool.h>
+// #include "../common/s21_bool.h"
+
+int print_file(FILE *file);
+
+typedef struct {
+  int opt_b;
+  int opt_e;
+  int opt_n;
+  int opt_s;
+  int opt_t;
+} Options;
 
 int main(int argc, char **argv) {
     int result = 0;
     int count = 1;
+    Options Flag;
+    Flag.opt_b = Flag.opt_e = Flag.opt_n = Flag.opt_s = Flag.opt_t = 0;
 
-    while (count <= argc) {
+
+    while (count < argc) {
         FILE *file;
         file = fopen(argv[count], "r");
-
-        if (file != NULL) {
-            int c;
-            while (s21_true) {
-                c = fgetc(file);
-                if (feof(file) || ferror(file)) break;
-                putchar(c);
-            }
-        } /*else {
-            result = 2;
-            printf("No such file %s\n", argv[count]);
-        }*/
-        fclose(file);
+        result = print_file(file);
         count++;
+        fclose(file);
     }
     return (result);
 }
 
+int print_file(FILE *file) {
+    int error = 0;
+    if (file != NULL) {
+        int c;
+        while (true) {
+            c = fgetc(file);
+            if (feof(file) || ferror(file)) break;
+            putchar(c);
+        }
+    } else {
+            error = 2;  // Раскоментировать в конце для корректной работы
+        puts("No such file");
+    }
+    return error;
+}
