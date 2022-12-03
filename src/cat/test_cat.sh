c1fdad07eac70baaa51c91475a84ee2ec76697e1
Yonn Argelia <yonnarge@student.21-school.ru>
2022-12-04 01:05:33 +0300

    Rewriting s21_cat.c. Little fix '--help'.

--- src/cat/test_cat.sh
+++ src/cat/test_cat.sh
@@ -1,32 +1,107 @@
 #!/bin/bash
 
-PATH_TO="../../datasets/"
-#FILE = bytes.txt
-FILE="first.txt"
-DIR_TRACES="_traces/"
-#flags=""
-#flags="-s"
-#flags="-b"
-#flags="-t"
- flags="-e"
-#flags="--show-nonprinting"
+COUNTER_SUCCESS=0
+COUNTER_FAIL=0
+DIFF_RES=""
+TEST_FILE="../../datasets/bytes.txt"
+echo "" > log.txt
 
-touch 42.txt
-rm *.txt
+for var in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+do
+          TEST1="$var $TEST_FILE"
+          echo "$TEST1"
+          ./s21_cat $TEST1 > s21_cat.txt
+          cat $TEST1 > cat.txt
+          DIFF_RES="$(diff -s s21_cat.txt cat.txt)"
+          if [ "$DIFF_RES" == "Files s21_cat.txt and cat.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST1" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_cat.txt cat.txt
+done
 
-clear
-make
+for var in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+do
+  for var2 in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+  do
+        if [ $var != $var2 ]
+        then
+          TEST1="$var $var2 $TEST_FILE"
+          echo "$TEST1"
+          ./s21_cat $TEST1 > s21_cat.txt
+          cat $TEST1 > cat.txt
+          DIFF_RES="$(diff -s s21_cat.txt cat.txt)"
+          if [ "$DIFF_RES" == "Files s21_cat.txt and cat.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST1" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_cat.txt cat.txt
+        fi
+  done
+done
 
-rm -rf $DIR_TRACES
-mkdir $DIR_TRACES
+for var in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+do
+  for var2 in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+  do
+      for var3 in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+      do
+        if [ $var != $var2 ] && [ $var2 != $var3 ] && [ $var != $var3 ]
+        then
+          TEST1="$var $var2 $var3 $TEST_FILE"
+          echo "$TEST1"
+          ./s21_cat $TEST1 > s21_cat.txt
+          cat $TEST1 > cat.txt
+          DIFF_RES="$(diff -s s21_cat.txt cat.txt)"
+          if [ "$DIFF_RES" == "Files s21_cat.txt and cat.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST1" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_cat.txt cat.txt
 
-echo "------------------------"
-echo "orig: cat $flags"
-cat $flags $PATH_TO$FILE
-echo "--------------"
-echo "s21_: s21_cat $flags"
-./s21_cat $flags $PATH_TO$FILE
-echo "--------------"
-cd $DIR_TRACES && cat $flags ../$PATH_TO$FILE > orig$flags.txt && ../s21_cat $flags ../$PATH_TO$FILE > s21$flags.txt
-diff -s orig$flags.txt s21$flags.txt
-echo "--------------"
+        fi
+      done
+  done
+done
+
+for var in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+do
+  for var2 in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+  do
+      for var3 in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+      do
+          for var4 in -b -e -n -s -t -v #-E -T --number-nonblank --number --squeeze-blank
+          do
+            if [ $var != $var2 ] && [ $var2 != $var3 ] && [ $var != $var3 ] && [ $var != $var4 ] && [ $var2 != $var4 ] && [ $var3 != $var4 ]
+            then
+              TEST1="$var $var2 $var3 $var4 $TEST_FILE"
+              echo "$TEST1"
+              ./s21_cat $TEST1 > s21_cat.txt
+              cat $TEST1 > cat.txt
+              DIFF_RES="$(diff -s s21_cat.txt cat.txt)"
+              if [ "$DIFF_RES" == "Files s21_cat.txt and cat.txt are identical" ]
+                then
+                  (( COUNTER_SUCCESS++ ))
+                else
+                  echo "$TEST1" >> log.txt
+                  (( COUNTER_FAIL++ ))
+              fi
+              rm s21_cat.txt cat.txt
+
+            fi
+          done
+      done
+  done
+done
+
+echo "SUCCESS: $COUNTER_SUCCESS"
+echo "FAIL: $COUNTER_FAIL"
